89b22d7fe19562708395e19c880b7324033b7795
Singh, Bhavya (UG - Comp Sci & Elec Eng) <bs01346@surrey.ac.uk>
2023-09-28 16:13:40 +0000

    Add a missing semicolon

--- ProblemSets/src/main/java/ProblemSet_4c/Student.java
+++ ProblemSets/src/main/java/ProblemSet_4c/Student.java
@@ -14,7 +14,7 @@ public class Student {
 	}
 
 	public String getStudentDetails() {
-		return initial + ". " + surname + " "
+		return initial + ". " + surname + " ";
 		 calculateYearAverage() + "%";
 	}
 
