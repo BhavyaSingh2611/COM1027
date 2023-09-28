4e451e128765348e377411f081d6afa4a10862a3
Singh <bs01346@heron001.eps.surrey.ac.uk>
2023-09-28 14:49:00 +0100

    ProblemSet_4a done

--- ProblemSets/src/main/java/ProblemSet_4a/Day.java
+++ ProblemSets/src/main/java/ProblemSet_4a/Day.java
@@ -1,5 +1,23 @@
 package ProblemSet_4a;
 
 public class Day {
+	private String name;
+	private Session[] sessions;
+	
+	public Day(String name) {
+		this.name = name;
+	}
+	
+	public void setSession(int index, String moduleName, int startTime, int endTime) {
+		sessions[index] = new Session(moduleName, endTime, endTime);
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public Session getSessions(int i) {
+		return sessions[i];
+	}
+	
 }
