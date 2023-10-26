a272ab1983902b936115c7817a81debaf744e6c5
Bhavya Singh <bhavyasingh2611@outlook.com>
2023-10-26 21:57:38 +0100

    finished 7a, 7b

---
+++ ProblemSets/src/main/java/ProblemSet_7a/Module.java
@@ -0,0 +1,20 @@
+package ProblemSet_7a;
+
+import java.util.regex.Pattern;
+
+public class Module {
+    private String name = null;
+    private static final String PATTERN = "COM\\d{4}";
+    public Module(String name) {
+        super();
+        this.name = name;
+        Pattern pattern = Pattern.compile(PATTERN);
+        if (!pattern.matcher(name).matches()) {
+           this.name = "Error";
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ ProblemSets/src/main/java/ProblemSet_7b/Student.java
@@ -0,0 +1,52 @@
+package ProblemSet_7b;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Student {
+    private String name = null;
+    private String urn = null;
+    private List<Module> moduleList;
+    private static final String NAME_PATTERN = "^[A-Z][a-z]* [A-Z][a-z]*$";
+    private static final String URN_PATTERN = "\\d{5}";
+    public Student(String name, String urn) {
+        super();
+        this.moduleList = new ArrayList<Module>();
+        this.name = name;
+        this.urn = urn;
+
+        Pattern namePattern = Pattern.compile(NAME_PATTERN);
+        Pattern urnPattern = Pattern.compile(URN_PATTERN);
+
+        if (!namePattern.matcher(name).matches()) {
+            //TODO: Check on the discussion forum
+            this.name = "Invalid Name";
+        }
+        if (!urnPattern.matcher(urn).matches()) {
+            this.urn = "00000";
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getUrn() {
+        return Integer.parseInt(this.urn);
+    }
+
+    public void addModuleList(Module module) {
+        if ((module != null) && (!this.moduleList.contains(module))) {
+            this.moduleList.add(module);
+        }
+    }
+
+    public String printModules() {
+        StringBuilder toReturn = new StringBuilder();
+        for (Module module : this.moduleList) {
+            toReturn.append(module.getName()).append(", ");
+        }
+        return toReturn.substring(0, toReturn.length() - 2);
+    }
+}
